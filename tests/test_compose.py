eb19f36d2274bb8943c5c91ef85af9c5efe05cc2
Yuxiang Gan <t-yugan@microsoft.com>
2018-07-03 16:42:14 +0800

    update expect file,delete test output file

--- tests/test_compose.py
+++ tests/test_compose.py
@@ -3,11 +3,11 @@ import json
 
 
 def test_compose():
-    with open('tests/test_compose_resources/deplyment_with_createoption.json') as json_file:
+    with open('test_compose_resources/deplyment_with_createoption.json') as json_file:
         json_data = json.load(json_file)
         proj = ComposeProject(json_data)
         proj.parse_services()
-        proj.dump('tests/test_compose_resources/docker-compose_test.yml')
-        actual_output  = open('tests/test_compose_resources/docker-compose_test.yml', 'r').read()
-        expected_output = open('tests/test_compose_resources/docker-compose.yml', 'r').read()
+        proj.dump('test_compose_resources/docker-compose_test.yml')
+        actual_output  = open('test_compose_resources/docker-compose_test.yml', 'r').read()
+        expected_output = open('test_compose_resources/docker-compose.yml', 'r').read()
         assert actual_output == expected_output
