5b10dde5bca4674a537dedc9a1e198389c0581ce
Ray Fang <fangzh@microsoft.com>
2018-07-03 21:44:29 +0800

    Update

--- iotedgehubdev/cli.py
+++ iotedgehubdev/cli.py
@@ -14,14 +14,6 @@ CONN_STR = 'connectionString'
 CERT_PATH = 'certPath'
 GATEWAY_HOST = 'gatewayhost'
 
-# @click.command()
-# @click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-# @click.argument('name', default='world', required=False)
-# def main(name, as_cowboy):
-#     """My Tool does one thing, and one thing well."""
-#     greet = 'Howdy' if as_cowboy else 'Hello'
-#     click.echo('{0}, {1}.'.format(greet, name))
-
 
 @click.group(context_settings=CONTEXT_SETTINGS, invoke_without_command=True)
 @click.version_option()
@@ -33,48 +25,53 @@ def main():
 
 
 @click.command(context_settings=CONTEXT_SETTINGS,
-               help='setup EdgeHub. This must be done before starting.')
+               help='Setup the EdgeHub runtime. This must be done before starting.')
 @click.option('--connection-string',
               '-c',
               required=True,
-              help='Provide the connection string of the edge device.')
+              help='Set the connection string of the Edge device. Note: Use double quotes when supplying this input.')
 @click.option('--gateway-host',
               '-g',
               required=False,
-              help='GatewayHostName value for the module to connect. Default is the FQDN of the device')
-def setup(connectionstring, gatewayhost):
-    if gatewayhost is None:
-        gatewayhost = Utils.get_hostname()
+              default=Utils.get_hostname(),
+              show_default=True,
+              help='GatewayHostName value for the module to connect.')
+def setup(connection_string, gateway_host):
     fileType = 'edgehub.config'
     certDir = HostPlatform.get_default_cert_path()
     Utils.mkdir_if_needed(certDir)
-    edgeCert = EdgeCert(certDir, gatewayhost)
+    edgeCert = EdgeCert(certDir, gateway_host)
     edgeCert.generate_self_signed_certs()
     configFile = HostPlatform.get_config_file_path()
     Utils.delete_file(configFile, fileType)
     Utils.mkdir_if_needed(HostPlatform.get_config_path())
     configDict = {
-        CONN_STR: connectionstring,
+        CONN_STR: connection_string,
         CERT_PATH: certDir,
-        GATEWAY_HOST: gatewayhost
+        GATEWAY_HOST: gateway_host
     }
     configJson = json.dumps(configDict, indent=2, sort_keys=True)
     Utils.create_file(configFile, configJson, fileType)
-    output.info('Setup EdgeHub successfully')
+    output.info('Setup EdgeHub runtime successfully')
 
 
 @click.command(context_settings=CONTEXT_SETTINGS,
-               help='get the connection string of target module.')
-@click.option('--islocal',
-              default=False,
+               help='Get the connection string of target module.')
+@click.option('--local',
+              '-l',
               required=False,
               is_flag=True,
-              help='if set `GatewayHostName` is `localhost` for module to run on host natively')
-@click.option('--outputfile',
+              default=False,
+              show_default=True,
+              help='Set `localhost` to `GatewayHostName` for module to run on host natively.')
+@click.option('--output',
+              '-o',
+              'output_file',  # non-user-facing alias to prevent conflict with the other variable named `output`
               required=False,
-              default=None,
-              help='use `--outputfile` to specify the output file to save the connection string')
-def targetconnstr(islocal, outputfile):
+              show_default=True,
+              help='Specify the output file to save the connection string.')
+#   TODO: better naming or shorter
+def targetconnstr(local, output_file):
     configFile = HostPlatform.get_config_file_path()
     if Utils.check_if_file_exists(configFile) is not True:
         output.error('Cannot find config file. Please setup first')
@@ -87,7 +84,7 @@ def targetconnstr(islocal, outputfile):
                 certPath = jsonObj[CERT_PATH]
                 gatewayhost = jsonObj[GATEWAY_HOST]
                 edgeManager = EdgeManager(connectionString, gatewayhost, certPath)
-                connstr = edgeManager.getOrAddModule('target', islocal)
+                connstr = edgeManager.getOrAddModule('target', local)
                 output.info('Target module connection string is {0}'.format(connstr))
             else:
                 output.error('Missing keys in config file. Please setup again')
@@ -96,16 +93,25 @@ def targetconnstr(islocal, outputfile):
 
 
 @click.command(context_settings=CONTEXT_SETTINGS,
-               help="start the EdgeHub runtime.")
+               help="Start the EdgeHub runtime.")
+@click.option('--compose',
+              '-c',
+              required=False,
+              is_flag=True,
+              default=False,
+              show_default=True,
+              help='Start EdgeHub runtime in Docker Compose mode.')
 @click.option('--inputs',
               '-i',
               required=False,
               default=None,
-              help='use `--inputs` to specify the input array of the target module')
-def start(inputs):
+              help='Specify the inputs of the target module, separated by commas.')
+#   TODO: Use multi?
+def start(compose, inputs):
+    print(single_mode)
     configFile = HostPlatform.get_config_file_path()
     if Utils.check_if_file_exists(configFile) is not True:
-        output.error('Cannot find config file. Please setup first')
+        output.error('Cannot find config file. Please run `iotedgehubdev setup` first.')
         return
 
     try:
@@ -119,19 +125,19 @@ def start(inputs):
                 gatewayhost = jsonObj[GATEWAY_HOST]
                 edgeManager = EdgeManager(connectionString, gatewayhost, certPath)
                 edgeManager.startForSingleModule(inputs)
-                output.info('EdgeHub has been started in single mode. Please connect your module as target and test')
+                output.info('EdgeHub runtime has been started in single mode. Please connect your module as target and test.')
             else:
-                output.error('Missing keys in config file. Please setup again')
+                output.error('Missing keys in config file. Please run `iotedgehubdev setup` again.')
     except Exception as e:
         output.error('Error: {0}'.format(str(e)))
 
 
 @click.command(context_settings=CONTEXT_SETTINGS,
-               help="stop the EdgeHub runtime.")
+               help="Stop the EdgeHub runtime.")
 def stop():
     try:
         EdgeManager.stop()
-        output.info('EdgeHub has been stopped successfully')
+        output.info('EdgeHub runtime has been stopped successfully')
     except Exception as e:
         output.error('Error: {0}'.format(str(e)))
 
