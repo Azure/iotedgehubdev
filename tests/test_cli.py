996a1e7571d2b1e078db45d418f27a43723463b3
v-cheye <v-cheye@microsoft.com>
2018-09-27 17:44:48 +0800

    update test code

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,7 +7,10 @@ from click.testing import CliRunner
 from iotedgehubdev import cli
 from iotedgehubdev import configs
 
-VALID_DEVICECONNECTIONSTRING='HostName=testhubiotedge.azure-devices.net;DeviceId=testiotedgedevice;SharedAccessKey=CDV2+VFefbpQWXDRdg7h483JngzYO2C0uRfB/ameF04='
+VALID_DEVICECONNECTIONSTRING = ('HostName=testhubiotedge.azure-devices.net;'
+                                'DeviceId=testiotedgedevice;'
+                                'SharedAccessKey=CDV2+VFefbpQWXDRdg7h483JngzYO2C0uRfB/ameF04=')
+
 
 @pytest.fixture
 def runner():
@@ -16,34 +19,35 @@ def runner():
     config.set('DEFAULT', 'firsttime', 'yes')
     configs._prod_config.update_config()
 
+
 def test_cli(runner):
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
     assert not result.exception
     assert 'Usage: main' in result.output.strip()
 
-@pytest.mark.skip(reason="there is permission issue here")
+
 def test_cli_setup(runner):
-    result = runner.invoke(cli.setup,['-c',VALID_DEVICECONNECTIONSTRING])
+    result = runner.invoke(cli.setup, ['-c', VALID_DEVICECONNECTIONSTRING])
     assert not result.exception
     assert result.exit_code == 0
     assert 'Setup IoT Edge Simulator successfully' in result.output.strip()
 
-@pytest.mark.skip(reason="there is permission issue here")
+
 def test_cli_modulecred(runner):
     result = runner.invoke(cli.modulecred)
     assert not result.exception
     assert result.exit_code == 0
     assert 'EdgeHubConnectionString' in result.output.strip()
 
-@pytest.mark.skip(reason="there is permission issue here")
+
 def test_cli_start(runner):
-    result = runner.invoke(cli.start,['-i','input1'])
+    result = runner.invoke(cli.start, ['-i', 'input1'])
     assert not result.exception
     assert result.exit_code == 0
     assert 'IoT Edge Simulator has been started in single module mode' in result.output.strip()
 
-@pytest.mark.skip(reason="there is permission issue here")
+
 def test_cli_stop(runner):
     result = runner.invoke(cli.stop)
     assert not result.exception
