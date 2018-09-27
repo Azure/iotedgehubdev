79ca0129c81f14491f3e56522df47306cc37b336
v-cheye <v-cheye@microsoft.com>
2018-09-27 10:05:15 +0800

    add test cases against cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,9 +2,22 @@
 # Licensed under the MIT License.
 
 
+import unittest
 import pytest
 from click.testing import CliRunner
 from iotedgehubdev import cli
+from iotedgehubdev import configs
+from iotedgehubdev import output
+
+VALID_DEVICECONNECTIONSTRING = 'HostName=testhubiotedge.azure-devices.net;DeviceId=testiotedgedevice;SharedAccessKey=CDV2+VFefbpQWXDRdg7h483JngzYO2C0uRfB/ameF04='
+PRINTER = output.Output()
+
+
+def teardown_module():
+    PRINTER.param("update setting.ini as firsttime", True, '[DEFAULT]->firsttime->yes', False)
+    config = configs._prod_config.config
+    config.set('DEFAULT', 'firsttime', 'yes')
+    configs._prod_config.update_config()
 
 
 @pytest.fixture
@@ -16,8 +29,35 @@ def test_cli(runner):
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
     assert not result.exception
-#     assert result.output.strip() == 'Hello, world.'
+    assert 'Usage: main' in result.output.strip()
 
+
+def test_cli_setup(runner):
+    result = runner.invoke(cli.setup, ['-c', VALID_DEVICECONNECTIONSTRING])
+    assert not result.exception
+    assert result.exit_code == 0
+    assert 'Setup IoT Edge Simulator successfully' in result.output.strip()
+
+
+def test_cli_modulecred(runner):
+    result = runner.invoke(cli.modulecred)
+    assert not result.exception
+    assert result.exit_code == 0
+    assert 'EdgeHubConnectionString' in result.output.strip()
+
+
+def test_cli_start(runner):
+    result = runner.invoke(cli.start, ['-i', 'input1'])
+    assert not result.exception
+    assert result.exit_code == 0
+    assert 'IoT Edge Simulator has been started in single module mode' in result.output.strip()
+
+
+def test_cli_stop(runner):
+    result = runner.invoke(cli.stop)
+    assert not result.exception
+    assert result.exit_code == 0
+    assert 'IoT Edge Simulator has been stopped successfully' in result.output.strip()
 
 # def test_cli_with_option(runner):
 #     result = runner.invoke(cli.main, ['--as-cowboy'])
