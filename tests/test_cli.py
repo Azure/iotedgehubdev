f2d5a915f361e33cb7e54e5623fe03b2dd334846
v-cheye <v-cheye@microsoft.com>
2018-11-13 15:57:12 +0800

    update by comment

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,19 +13,33 @@ from iotedgehubdev import configs
 from iotedgehubdev.edgedockerclient import EdgeDockerClient
 from iotedgehubdev.hostplatform import HostPlatform
 
+VALID_DEVICECONNECTIONSTRING = os.environ['DEVICE_CONNECTION_STRING']
+VALID_IOTHUBCONNECTIONSTRING = os.environ['IOTHUB_CONNECTION_STRING']
+
+device_id = re.findall(".*DeviceId=(.*);SharedAccessKey.*", VALID_DEVICECONNECTIONSTRING)[0]
+iothub_name = re.findall(".*HostName=(.*);DeviceId.*", VALID_DEVICECONNECTIONSTRING)[0].split('.')[0]
 workingdirectory = os.getcwd()
 filename = os.path.join(workingdirectory, '.env')
 if os.path.exists(filename):
     load_dotenv(filename)
 docker_client = EdgeDockerClient()
 
-VALID_DEVICECONNECTIONSTRING = os.environ['DEVICE_CONNECTION_STRING']
-VALID_IOTHUBCONNECTIONSTRING = os.environ['IOTHUB_CONNECTION_STRING']
-
 
 @pytest.fixture
 def runner():
     return CliRunner()
+
+
+def update_setting_ini_as_firsttime():
+    config = configs._prod_config.config
+    config.set('DEFAULT', 'firsttime', 'yes')
+    configs._prod_config.update_config()
+
+
+def cli_setup(runner):
+    result = runner.invoke(cli.setup, ['-c', VALID_DEVICECONNECTIONSTRING, '-g', 'iotedgetestingnow'])
+    if 'Setup IoT Edge Simulator successfully' not in result.output.strip():
+        raise 'It failed to setup iotedgehubdev.'
 
 
 def start_process(command, is_shell):
@@ -35,46 +49,28 @@ def start_process(command, is_shell):
     if process.returncode == 0:
         print('Process Output Message:\n' + str(output))
     else:
-        print('Process Error Message:\n' + str(error))
+        raise 'Process Error Message:\n' + str(error)
     return str(output)
-
-
-def cli_setup(runner):
-    result = runner.invoke(cli.setup, ['-c', VALID_DEVICECONNECTIONSTRING, '-g', 'iotedgetestingnow'])
-    assert 'Setup IoT Edge Simulator successfully' in result.output.strip()
 
 
 def cli_start_with_deployment(runner, deployment_file):
     test_resources_dir = os.path.join('tests', 'test_compose_resources')
     deployment_json_file_path = os.path.join(test_resources_dir, deployment_file)
     result = runner.invoke(cli.start, ['-d', deployment_json_file_path])
-    assert 'IoT Edge Simulator has been started in solution mode' in result.output.strip()
-
-
-def get_device_id_from_device_connection_string():
-    device_id = re.findall(".*DeviceId=(.*);SharedAccessKey.*", VALID_DEVICECONNECTIONSTRING)[0]
-    return device_id
-
-
-def get_iothub_name_from_device_connection_string():
-    iothub_name = re.findall(".*HostName=(.*);DeviceId.*", VALID_DEVICECONNECTIONSTRING)[0].split('.')[0]
-    return iothub_name
+    if 'IoT Edge Simulator has been started in solution mode' not in result.output.strip():
+        raise 'It failed to start with deployment file.'
 
 
 def invoke_module_method():
-    device_id = get_device_id_from_device_connection_string()
-    iothub_name = get_iothub_name_from_device_connection_string()
     invoke_module_method_cmd = 'az iot hub invoke-module-method --device-id "' + device_id + \
         '" --method-name "reset" --module-id "tempSensor" --hub-name "' + \
         iothub_name + '" --login "' + VALID_IOTHUBCONNECTIONSTRING + '"'
     output = start_process(invoke_module_method_cmd, True)
-    if '"status": 200' not in output:
+    if '"status": 200' not in str(output):
         raise "Failed to invoke module method."
 
 
 def monitor_d2c_message():
-    device_id = get_device_id_from_device_connection_string()
-    iothub_name = get_iothub_name_from_device_connection_string()
     invoke_monitor_events_cmd = 'az iot hub monitor-events -n "' + iothub_name + \
         '" -d "' + device_id + '" --login "' + VALID_IOTHUBCONNECTIONSTRING + '" -y -t 5'
     output = start_process(invoke_monitor_events_cmd, True)
@@ -83,16 +79,10 @@ def monitor_d2c_message():
 
 def cli_stop(runner):
     result = runner.invoke(cli.stop)
-    assert not result.exception
-    assert result.exit_code == 0
-    assert 'IoT Edge Simulator has been stopped successfully' in result.output.strip()
-
-
-def reset_docker_environment():
-    start_process(['docker', 'network', 'rm', 'azure-iot-edge-dev'], False)
-    start_process(['docker', 'rmi', '-f', 'mcr.microsoft.com/azureiotedge-simulated-temperature-sensor:1.0'], False)
-    start_process(['docker', 'rmi', '-f', 'mcr.microsoft.com/azureiotedge-hub:1.0'], False)
-    start_process(['docker', 'rmi', '-f', 'hello-world'], False)
+    if result.exit_code == 0:
+        return result
+    else:
+        raise 'Failed to stop iotedgehubdev. Error Message: \n' + result.exception + '\n'
 
 
 def verify_docker_output(docker_cmd, expect_values):
@@ -117,14 +107,19 @@ def wait_verify_docker_output(docker_cmd, expect_values):
             raise "timeout to wait until it appears expected value " + expect_values + "... ...\n"
 
 
-def remove_docker_volume(volume_name):
-    start_process(['docker', 'volume', 'rm', volume_name, '-f'], False)
+def remove_docker_volumes(volumes):
+    for volume in volumes:
+        start_process(['docker', 'volume', 'rm', volume, '-f'], False)
 
 
-def update_setting_ini_as_firsttime():
-    config = configs._prod_config.config
-    config.set('DEFAULT', 'firsttime', 'yes')
-    configs._prod_config.update_config()
+def remove_docker_images(images):
+    for image in images:
+        start_process(['docker', 'rmi', '-f', image], False)
+
+
+def remove_docker_networks(networks):
+    for network in networks:
+        start_process(['docker', 'network', 'rm', network], False)
 
 
 def test_cli(runner):
@@ -144,12 +139,15 @@ def test_cli_start_with_deployment(runner):
         invoke_module_method()
         wait_verify_docker_output(['docker', 'logs', 'tempSensor'], ['Received direct method'])
         output = monitor_d2c_message()
-        device_id = get_device_id_from_device_connection_string()
         assert 'origin: ' + device_id in str(output)
         assert '{"machine":{"temperature":' in str(output)
     finally:
-        cli_stop(runner)
-        reset_docker_environment()
+        result = cli_stop(runner)
+        assert 'IoT Edge Simulator has been stopped successfully' in result.output.strip()
+        remove_docker_networks(['azure-iot-edge-dev'])
+        remove_docker_images(['mcr.microsoft.com/azureiotedge-simulated-temperature-sensor:1.0',
+                              'mcr.microsoft.com/azureiotedge-hub:1.0',
+                              'hello-world'])
 
 
 def test_cli_setup_during_first_time(runner):
@@ -166,7 +164,6 @@ def test_cli_setup_during_first_time(runner):
 
 
 def test_cli_modulecred(runner):
-    iothub_name = get_iothub_name_from_device_connection_string()
     cli_setup(runner)
     result = runner.invoke(cli.modulecred)
     assert not result.exception
@@ -178,7 +175,6 @@ def test_cli_modulecred(runner):
 
 def test_cli_output_modulecred_file(runner):
     try:
-        iothub_name = get_iothub_name_from_device_connection_string()
         cli_setup(runner)
         modulered_output_file = 'modulecred_output.txt'
         result = runner.invoke(cli.modulecred, ['-o', modulered_output_file])
@@ -200,18 +196,24 @@ def test_cli_output_modulecred_file(runner):
 def test_cli_create_options_for_custom_volume(runner):
     try:
         cli_setup(runner)
-        remove_docker_volume('testVolume')
+        remove_docker_volumes(['testVolume'])
         cli_start_with_deployment(runner, 'deployment_with_custom_volume.json')
-        wait_verify_docker_output(['docker', 'volume', 'ls'], ['testVolume'])
+        wait_verify_docker_output(['docker', 'volume', 'ls'], ['testVolume', 'edgemoduledev', 'edgehubdev'])
         expected_volumes = (['"Source": "testVolume"',
                              '"Destination": "/mnt_test"',
                              '"Source": "edgemoduledev"',
                              '"Destination": "/mnt/edgemodule"'])
         wait_verify_docker_output(['docker', 'inspect', 'tempSensor'], expected_volumes)
+        wait_verify_docker_output(['docker', 'inspect', 'edgehubdev'],
+                                  '"Mountpoint": "/var/lib/docker/volumes/edgehubdev/_data"')
     finally:
-        cli_stop(runner)
-        reset_docker_environment()
-        remove_docker_volume('testVolume')
+        result = cli_stop(runner)
+        assert 'IoT Edge Simulator has been stopped successfully' in result.output.strip()
+        remove_docker_networks(['azure-iot-edge-dev'])
+        remove_docker_images(['mcr.microsoft.com/azureiotedge-simulated-temperature-sensor:1.0',
+                              'mcr.microsoft.com/azureiotedge-hub:1.0',
+                              'hello-world'])
+        remove_docker_volumes(['testVolume'])
 
 
 def test_cli_modulecred_for_multiple_modules(runner):
@@ -232,8 +234,12 @@ def test_cli_start_with_chunked_create_options(runner):
         expect_createoptions = ['FOO1=bar1', 'FOO2=bar2', 'FOO3=bar3', 'FOO4=bar4', 'FOO5=bar5', 'FOO6=bar6', 'FOO7=bar7']
         wait_verify_docker_output(['docker', 'inspect', 'tempSensor'], expect_createoptions)
     finally:
-        cli_stop(runner)
-        reset_docker_environment()
+        result = cli_stop(runner)
+        assert 'IoT Edge Simulator has been stopped successfully' in result.output.strip()
+        remove_docker_networks(['azure-iot-edge-dev'])
+        remove_docker_images(['mcr.microsoft.com/azureiotedge-simulated-temperature-sensor:1.0',
+                              'mcr.microsoft.com/azureiotedge-hub:1.0',
+                              'hello-world'])
 
 
 def test_cli_start_with_input(runner):
@@ -244,8 +250,11 @@ def test_cli_start_with_input(runner):
         assert 'IoT Edge Simulator has been started in single module mode' in result.output.strip()
         assert 'curl --header' in result.output.strip()
     finally:
-        cli_stop(runner)
-        reset_docker_environment()
+        result = cli_stop(runner)
+        assert 'IoT Edge Simulator has been stopped successfully' in result.output.strip()
+        remove_docker_networks(['azure-iot-edge-dev'])
+        remove_docker_images(['mcr.microsoft.com/azureiotedge-hub:1.0',
+                              'mcr.microsoft.com/azureiotedge-testing-utility:1.0.0-rc1'])
 
 
 # def test_cli_with_option(runner):
