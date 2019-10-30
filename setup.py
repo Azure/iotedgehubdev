df3f6106094be86618dfe9b2e015fe3896a49fb7
Renlong Tu <5545529+SLdragon@users.noreply.github.com>
2019-10-30 06:18:53 +0100

    Add python version requirement in README and setup.py (#232)

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
             'iotedgehubdev = iotedgehubdev.cli:main',
         ],
     },
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <3.8',
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         # 'Development Status :: 1 - Planning',
@@ -75,7 +76,12 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
