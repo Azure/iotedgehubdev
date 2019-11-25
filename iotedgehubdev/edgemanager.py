8a02a60cf2d13fade7cd68a2c03ef3de4661aa26
rentu <rentu@microsoft.com>
2019-11-25 03:54:05 +0100

    Fix line too long issue for pylint

--- iotedgehubdev/edgemanager.py
+++ iotedgehubdev/edgemanager.py
@@ -297,7 +297,8 @@ class EdgeManager(object):
                     return self.addModule(name, islocal)
                 except ResponseError as adderr:
                     if adderr.status_code == 400:
-                        raise ResponseError(400, adderr.value + " Please make sure you're using an Edge device instead of non-Edge device.")
+                        raise ResponseError(400, adderr.value + 
+                            " Please make sure you're using an Edge device instead of non-Edge device.")
                     raise adderr
             else:
                 raise geterr
