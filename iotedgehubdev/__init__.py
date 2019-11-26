954bcd4c47fecb6194dbb9d1419979a1eaedf4a8
rentu <rentu@microsoft.com>
2019-11-26 07:04:38 +0100

    0.12.0rc1

--- iotedgehubdev/__init__.py
+++ iotedgehubdev/__init__.py
@@ -8,5 +8,5 @@ import six  # noqa: F401
 pkg_resources.declare_namespace(__name__)
 
 __author__ = 'Microsoft Corporation'
-__version__ = '0.12.0rc0'
+__version__ = '0.12.0rc1'
 __AIkey__ = '95b20d64-f54f-4de3-8ad5-165a75a6c6fe'
