f59b3dc91d44f29d6cac2bdde8ab2c37d49ed23c
Haitham Shami <hshami@microsoft.com>
2021-04-22 11:41:58 -0700

    update version number and changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ Azure IoT EdgeHub Dev Tool
 """
 from setuptools import find_packages, setup
 
-VERSION = '0.14.8rc1'
+VERSION = '0.14.8'
 # If we have source, validate that our version numbers match
 # This should prevent uploading releases with mismatched versions.
 try:
