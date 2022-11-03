f4cab9060e6bb97d86416fb57b6b75c9c88b3aa5
Mariana Mihova <marine@microsoft.com>
2022-11-03 13:16:32 -0700

    Rename telemetry event as the schema is different

--- iotedgehubdev/__init__.py
+++ iotedgehubdev/__init__.py
@@ -7,6 +7,6 @@ import pkg_resources
 pkg_resources.declare_namespace(__name__)
 
 __author__ = 'Microsoft Corporation'
-__version__ = '0.14.17'
+__version__ = '0.14.18'
 __AIkey__ = '95b20d64-f54f-4de3-8ad5-165a75a6c6fe'
 __production__ = 'iotedgehubdev'
