98a7443f7a518e12fee49f7fb1fd3169ba396d65
Drew Hardie <drewhardie@microsoft.com>
2026-07-09 10:07:53 -0700

    Bump version to 0.14.19 and cap cryptography < 47

--- iotedgehubdev/__init__.py
+++ iotedgehubdev/__init__.py
@@ -2,6 +2,6 @@
 # Licensed under the MIT License.
 
 __author__ = 'Microsoft Corporation'
-__version__ = '0.14.18'
+__version__ = '0.14.19'
 __AIkey__ = '95b20d64-f54f-4de3-8ad5-165a75a6c6fe'
 __production__ = 'iotedgehubdev'
